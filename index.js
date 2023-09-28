64e4afa7cbfa8d84bfc4e3f45d54d08e3fb6fade
Agustin Carrizo <jcarrizoagustin@gmail.com>
2023-09-27 21:36:22 -0300

    modificando index.js

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ console.log('Archivo index');
 let num = 12;
 let num1 = 12
 console.log(num+num1);
+
+
+function contarCantidadDeLetras(palabra){
+    return palabra.length
+}
+
+console.log(contarCantidadDeLetras('Aeropuerto'))
